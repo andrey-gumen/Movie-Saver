942e59e47678b6465de580d37de36142c5df8a44
a.gumen <a.gumen@revizto.com>
2022-11-20 23:55:19 +0100

    create simple layout for MainListView

--- Movie Saver/GalleryFlow/MainListScreen/MainListView.swift
+++ Movie Saver/GalleryFlow/MainListScreen/MainListView.swift
@@ -1,12 +1,98 @@
-import UIKit
 import Combine
+import EasyAutolayout
+import UIKit
 
 final class MainListView: UIViewController {
+
+    // MARK: - Properties
     var viewModel: MainListViewModel!
+
+    // MARK: Public
+    // MARK: Private
     private var cancellables: Set<AnyCancellable> = []
-    
+
+    private let titleLabel = UILabel()
+    private let movieTableView = UITableView()
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+
+        addSubview()
+        configureUI()
+        configureConstraints()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        configureToolbar()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        restoreToolbar()
+    }
+
+    // MARK: - API
+    // MARK: - Setups
+    private func addSubview() {
+        view.addSubview(titleLabel)
+        view.addSubview(movieTableView)
+    }
+
+    private func configureUI() {
+        view.backgroundColor = ColorScheme.tableViewBackground
+
+        titleLabel.text = "My Moview List"
+        
+        movieTableView.backgroundColor = ColorScheme.tableViewBackground
+        movieTableView.delegate = self
+        movieTableView.dataSource = self
+    }
+
+    private func configureConstraints() {
+        titleLabel.pin
+            .top(to: view.safeAreaLayoutGuide)
+            .leading(to: view, offset: 16)
+            .trailing(to: view, offset: 16)
+            .height(to: 41)
+
+        movieTableView.pin
+            .below(of: titleLabel, offset: 30)
+            .leading(to: view)
+            .trailing(to: view)
+            .bottom(to: view.safeAreaLayoutGuide)
+    }
+
+    private func configureToolbar() {
+        let barButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonDidTapped))
+        navigationItem.setRightBarButton(barButton, animated: false)
+        navigationController?.isToolbarHidden = false
+    }
+
+    private func restoreToolbar() {
+        navigationItem.setRightBarButton(nil, animated: false)
+        navigationController?.isToolbarHidden = true
+    }
+
+    // MARK: - Helpers
+    @objc private func addButtonDidTapped() {
+        print(#function)
+    }
+}
+
+// MARK: table view
+
+extension MainListView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = ColorScheme.cellBackground
+        return cell
     }
 }
