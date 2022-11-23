f29d3dca78d569f8011bda02408caa7c7aba3833
a.gumen <a.gumen@revizto.com>
2022-11-23 23:08:15 +0100

    add finalizer for AddNewCoordinator

--- Movie Saver/AddNewFlow/AddNewCoordinator.swift
+++ Movie Saver/AddNewFlow/AddNewCoordinator.swift
@@ -5,16 +5,25 @@ final class AddNewCoordinator {
     
     let rootNavigationController: UINavigationController
     private var cancellables: Set<AnyCancellable> = []
+    private var flowFinalizer: (() -> Void)?
     
     init(_ rootNavigationController: UINavigationController) {
         self.rootNavigationController = rootNavigationController
     }
     
-    func start() {
+    func start(flowFinalizer: @escaping () -> Void) {
+        self.flowFinalizer = flowFinalizer
+        
         let viewModel = AddNewViewModel()
         let view = AddNewView()
         view.viewModel = viewModel
         rootNavigationController.pushViewController(view, animated: true)
+        
+        viewModel.viewDidDisapearSubject
+            .sink { [weak self] in
+                self?.flowFinalizer?()
+            }
+            .store(in: &cancellables)
     }
     
 }
--- Movie Saver/SceneCoordinator.swift
+++ Movie Saver/SceneCoordinator.swift
@@ -5,7 +5,7 @@ final class SceneCoordinator {
     
     private let rootNavigationController: UINavigationController
     private var coordinators: [AnyObject] = []
-        
+    private var cancellables: Set<AnyCancellable> = []
     
     init(_ rootNavigationController: UINavigationController) {
         self.rootNavigationController = rootNavigationController
@@ -13,9 +13,25 @@ final class SceneCoordinator {
     
     func start() {
         let coordinator = GalleryCoordinator(rootNavigationController)
-        coordinator.start()
+        
+        coordinator.showAddNewFlowSubject
+            .sink { [weak self] in
+                self?.showAddNewFlow()
+            }
+            .store(in: &cancellables)
         
         coordinators.append(coordinator)
+        coordinator.start()
+    }
+    
+    func showAddNewFlow() {
+        let coordinator = AddNewCoordinator(rootNavigationController)
+        
+        coordinators.append(coordinator)
+        coordinator.start() { [weak self] in
+            // remove AddNewCoordinator from memory
+            let _ = self?.coordinators.removeLast()
+        }
     }
     
 }
