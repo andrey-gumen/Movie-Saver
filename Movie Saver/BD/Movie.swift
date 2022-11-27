50ddb9558e0fe52f57b98a9d2db0758d00ad732d
a.gumen <a.gumen@revizto.com>
2022-11-28 00:28:22 +0100

    update movie model structure

--- Movie Saver/BD/Movie.swift
+++ Movie Saver/BD/Movie.swift
@@ -3,9 +3,9 @@ import UIKit
 
 struct Movie {
     let poster: UIImage?
-    let title: String
-    let releaseDate: Date
-    let rating: Float
-    let youtubeLink: String?
+    let title: String?
+    let releaseDate: Date?
+    let rating: Float?
+    let youtubeLink: URL?
     let notes: String?
 }
