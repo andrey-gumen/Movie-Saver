b653e5fe501b4ff31727c893f0c75bb41e60ace1
a.gumen <a.gumen@revizto.com>
2022-11-28 00:29:24 +0100

    implement get movies method

--- Movie Saver/BD/CoreDataManager.swift
+++ Movie Saver/BD/CoreDataManager.swift
@@ -30,4 +30,35 @@ final class CoreDataManager {
             print("ERROR")
         }
     }
+    
+    func getMovies() -> [Movie] {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return [] }
+
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "MovieEntity")
+
+        do {
+            let objects = try managedContext.fetch(fetchRequest)
+            var movies = [Movie]()
+            for object in objects {
+                let poster = object.value(forKey: "poster") as? Data
+                
+                let movie = Movie(
+                    poster: poster != nil ? UIImage(data: poster!) : nil,
+                    title: object.value(forKey: "title") as? String,
+                    releaseDate: object.value(forKey: "releaseDate") as? Date,
+                    rating: object.value(forKey: "rating") as? Float,
+                    youtubeLink: object.value(forKey: "youtubeLink") as? URL,
+                    notes: object.value(forKey: "title") as? String
+                )
+                movies.append(movie)
+            }
+            return movies
+        } catch let error as NSError {
+            print(error)
+        }
+
+        return []
+
+    }
 }
