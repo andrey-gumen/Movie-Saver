e95b58b3524ebdc4739b5fab33baebe02ec0383d
a.gumen <a.gumen@revizto.com>
2022-11-28 00:28:49 +0100

    implement save movie method

--- Movie Saver/BD/CoreDataManager.swift
+++ Movie Saver/BD/CoreDataManager.swift
@@ -6,4 +6,28 @@ final class CoreDataManager {
     
     private init() {}
     
+    func save(movie: Movie) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        if let entity = NSEntityDescription.entity(forEntityName: "MovieEntity", in: managedContext) {
+            
+            let storage = NSManagedObject(entity: entity, insertInto: managedContext)
+            storage.setValue(movie.title, forKey: "title")
+            storage.setValue(movie.releaseDate, forKey: "releaseDate")
+            storage.setValue(movie.rating, forKey: "rating")
+            storage.setValue(movie.youtubeLink, forKey: "youtubeLink")
+            storage.setValue(movie.poster?.pngData(), forKey: "poster")
+            storage.setValue(movie.notes, forKey: "notes")
+            
+            do {
+                try managedContext.save()
+            } catch let error as NSError {
+                print(error)
+            }
+            
+        } else {
+            print("ERROR")
+        }
+    }
 }
